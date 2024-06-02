6c64c6be4a94f62a7a9b7469882e9a45810bda73
Nephtys <lcamerly@student.42lyon.fr>
2024-06-02 15:26:17 +0200

    We can moove now

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lcamerly <lcamerly@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 22:54:08 by jules             #+#    #+#             */
-/*   Updated: 2024/06/02 09:16:50 by lcamerly         ###   ########.fr       */
+/*   Updated: 2024/06/02 15:09:50 by lcamerly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,43 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
-
-int	input(int key, t_minirt *minirt)
-{
-	if (key == XK_Escape)
-		close_minirt(minirt);
-	return (0);
-}
-
 int	draw_loop(t_minirt *minirt)
 {
 	if (render_scene(minirt))
 		close_minirt(minirt);
 	mlx_put_image_to_window(minirt->mlx, minirt->mlx_win, minirt->img.img, 0, \
 		0);
+	return (0);
+}
+
+int	input(int key, t_minirt *minirt)
+{
+	if (key == XK_Escape)
+		close_minirt(minirt);
+	if (key == XK_z)
+	{
+		minirt->scene->cam.pos = add(minirt->scene->cam.pos, \
+			scale(minirt->scene->cam.forward, 10.f));
+		minirt->scene->should_render = 1;
+	}
+	if (key == XK_s)
+	{
+		minirt->scene->cam.pos = sub(minirt->scene->cam.pos, \
+			scale(minirt->scene->cam.forward, 10.f));
+		minirt->scene->should_render = 1;
+	}
+	if (key == XK_q)
+	{
+		minirt->scene->cam.pos = sub(minirt->scene->cam.pos, \
+			scale(minirt->scene->cam.right, 10.f));
+		minirt->scene->should_render = 1;
+	}
+	if (key == XK_d)
+	{
+		minirt->scene->cam.pos = add(minirt->scene->cam.pos, \
+			scale(minirt->scene->cam.right, 10.f));
+		minirt->scene->should_render = 1;
+	}
 	return (0);
 }
 
